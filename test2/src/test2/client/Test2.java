e5d48fa68db0507bc5b13a10899ed0815017b857
kirkh3021@gmail.com <kirkh3021@gmail.com>
2013-11-15 15:02:43 -0500

    test2

--- test2/src/test2/client/Test2.java
+++ test2/src/test2/client/Test2.java
@@ -23,7 +23,7 @@ import com.google.gwt.user.client.ui.VerticalPanel;
 public class Test2 implements EntryPoint {
 	/**
 	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.kirk
+	 * returns an error.kirk  version 1.0
 	 */
 	private static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
