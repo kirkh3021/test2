1ccb210bbd4af1fad58628879d90a5230a4742ac
kirkh3021@gmail.com <kirkh3021@gmail.com>
2013-11-18 17:07:14 -0500

    final daily commit

--- test2/src/test2/client/GreetingServiceAsync.java
+++ test2/src/test2/client/GreetingServiceAsync.java
@@ -3,7 +3,7 @@ package test2.client;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
- * The async counterpart of <code>GreetingService</code>.
+ * Final kirk push on 11/18 The async counterpart of <code>GreetingService</code>.
  */
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback)
